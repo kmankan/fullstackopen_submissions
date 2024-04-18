32cc228c25da058910670aa764d12f92b0b8dabf
kmankan <malink027@gmail.com>
2024-04-18 13:36:06 +1000

    create app that lets user select feedback and returns statistics about feedback. Create seperate component to display statistics.

--- part1/feedback/src/App.jsx
+++ part1/feedback/src/App.jsx
@@ -23,55 +23,102 @@ const Button = ({ handleClick, text}) => (
   </button>
 )
 
+const StatisticLine = ({value, text}) => {
+  const [spaces, setSpaces] = useState(4);
+
+  return (
+    <div>
+      {text}
+      {'\u00a0'.repeat(spaces)} 
+      {value}
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  const good = props.good
+  const neutral = props.neutral
+  const bad = props.bad
+  const total = props.total
+  const average = (props.good - props.bad) / props.total
+  const positive = `${props.good / props.total * 100} %`
+
+  if (props.total === 0) {
+    return (
+      <div>
+        <br></br>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      <StatisticLine text="good" value={good}/>
+      <StatisticLine text="neutral" value={neutral}/>
+      <StatisticLine text="bad" value={bad}/>
+      <StatisticLine text="total" value={total}/>
+      <StatisticLine text="average" value={average}/>
+      <StatisticLine text="positive" value={positive}/>
+    </div>
+  )  
+}
+
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  // const [allClicks, setAll] = useState([])
 
   const [total, setTotal] = useState(0)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
 
-    setTotal(updatedLeft + right) 
+    setTotal(updatedGood + neutral + bad) 
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
 
-    setTotal(left + updatedRight)
+    setTotal(good + updatedNeutral + bad) 
+  }
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+
+    setTotal(good + neutral + updatedBad) 
   }
 
   const handleReset = () => {
-    setAll([])
-    setLeft(0)
-    setRight(0)
-
+    setGood(0)
+    setNeutral(0)
+    setBad(0)
     setTotal(0)
   }
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
+      <h1>
+        give feedback
+      </h1>
+      
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <br></br>
+      <br></br>
       <Button handleClick={handleReset} text='reset' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks}/>
-      total: {total}
+      
+      <h1>
+        statistics
+      </h1>
+
+      <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
     </div>
   )
 }
 
 export default App
-
-
-// const handleLeftClick = () =>
-// setClicks({ ...clicks, left: clicks.left + 1 })
-
-// const handleRightClick = () =>
-// setClicks({ ...clicks, right: clicks.right + 1 })
